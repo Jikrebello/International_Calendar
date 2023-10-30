f955c4fa4960a68ea1a105abba1b6f669ccf059c
James Rebello <Jikrebello.za@gmail.com>
2023-10-30 11:14:14 +0200

    Tried and failed to implement tests

--- API/Program.cs
+++ API/Program.cs
@@ -4,95 +4,104 @@ using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
-// Add DbContext.
-builder.Services.AddDbContext<DataContext>(
-    options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString(name: "DefaultConnection"))
-);
-
-builder.Services.AddServices();
-builder.Services.AddRepositories();
-
-builder.Services.AddIdentityServices(builder.Configuration);
-
-// Add Nuget Middleware.
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.AddSecurityDefinition(
-        name: "Bearer",
-        new OpenApiSecurityScheme
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        // Add DbContext.
+        builder.Services.AddDbContext<DataContext>(
+            options =>
+                options.UseSqlite(
+                    builder.Configuration.GetConnectionString(name: "DefaultConnection")
+                )
+        );
+
+        builder.Services.AddServices();
+        builder.Services.AddRepositories();
+
+        builder.Services.AddIdentityServices(builder.Configuration);
+
+        // Add Nuget Middleware.
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(c =>
         {
-            In = ParameterLocation.Header,
-            Description = "Please insert JWT with Bearer into field",
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
-        }
-    );
-    c.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
+            c.AddSecurityDefinition(
+                name: "Bearer",
                 new OpenApiSecurityScheme
                 {
-                    Reference = new OpenApiReference
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                }
+            );
+            c.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
                     {
-                        Id = "Bearer",
-                        Type = ReferenceType.SecurityScheme
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        Array.Empty<string>()
                     }
-                },
-                Array.Empty<string>()
-            }
+                }
+            );
+        });
+
+        builder.Services.AddCors();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    );
-});
+        else
+        {
+            app.UseCors(
+                builder =>
+                    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+            );
+        }
 
-builder.Services.AddCors();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseMiddleware<ExceptionMiddleware>();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseCors(
-        builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
-    );
-}
-
-// Add the CSP middleware here
-app.Use(
-    async (context, next) =>
-    {
-        context.Response.Headers.Add(
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; img-src *; media-src media1.com media2.com; script-src scripts.com"
+        // Add the CSP middleware here
+        app.Use(
+            async (context, next) =>
+            {
+                context.Response.Headers.Add(
+                    key: "Content-Security-Policy",
+                    value: "default-src 'self'; img-src *; media-src media1.com media2.com; script-src scripts.com"
+                );
+                await next();
+            }
         );
-        await next();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
-);
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+}
---
+++ International_Calendar.Test/UsersControllerTests.cs
@@ -0,0 +1,4 @@
+namespace International_Calendar.Test
+{
+    public class UsersControllerTests : WebApplicationFactory<Program>, IAsyncLifetime { }
+}
